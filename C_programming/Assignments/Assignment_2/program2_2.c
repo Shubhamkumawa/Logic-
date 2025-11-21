e83248885f6017140d5e3390338b311d421fd196
Shubham Kumawat <shubhamskumawat88@gmail.com>
2025-11-21 11:09:00 +0530

    Update program2_2.c

--- C_programming/Assignments/Assignment_2/program2_2.c
+++ C_programming/Assignments/Assignment_2/program2_2.c
@@ -1,3 +1,15 @@
+/////////////////////////////////////////////////////////
+//
+//  Function Name : Accept
+//  Description   : Printing the number of * taken from user
+//  Input         : Integer 
+//  Output        : Prints n number of characters
+//  Author        : Shubham Shankarlal kumawat
+//  Date          : 11/11/2025
+//
+/////////////////////////////////////////////////////////
+
+
 #include<stdio.h>
 
 void Display(int iNo)
@@ -20,3 +32,9 @@ int main()
     return 0 ;
 
 }
+
+/////////////////////////////////////////////////////////
+//
+//  Input: 5        Output: * * * * * 
+//
+/////////////////////////////////////////////////////////
