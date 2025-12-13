1c6a8bc0211364e4f315f225161bd0b2af736fd0
Shubham Kumawat <shubhamskumawat88@gmail.com>
2025-12-13 17:08:20 +0530

    Add files via upload

---
+++ C_programming/Assignments/Assignment_32/program32_3.c
@@ -0,0 +1,73 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   Display the pattern of character
+//  Input :         Integer
+//  output :        Void
+//  Author :        Shubham Shankarlal Kumawat
+//  Date :          26/11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>                                              
+
+void Display(int iRow , int iCol)
+{
+    int iCnt1 = 0 ;
+    int iCnt2 = 0 ;
+    if(iRow != iCol)
+        {
+            printf("Invalid input");
+            printf("Row number and column number should be same");
+            return;
+        }
+    for(iCnt1 = iRow ; iCnt1 >= 1 ; iCnt1--)
+    {
+        
+        for(iCnt2 = 1; iCnt2 <= iCol ; iCnt2++)
+        {
+            if(iCnt1 == iCnt2 || iCnt1 ==iRow  ||iCnt2== iCol|| iCnt1 == 1 || iCnt2 == 1 )
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("\t");
+            }
+            
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    printf("Enter the number of rows:");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns:");
+    scanf("%d", &iValue2);
+
+    Display(iValue1 , iValue2);
+
+    return 0 ;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:4    Input2:4   Output:*       *       *       *
+//                                *               *       *
+//                                *       *               *
+//                                *       *       *       *                   
+//
+//  Input1:6    Input2:6   Output:*       *       *       *       *       *
+//                                *                               *       *
+//                                *                       *               *
+//                                *               *                       *
+//                                *       *                               *
+//                                *       *       *       *       *       *             
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
