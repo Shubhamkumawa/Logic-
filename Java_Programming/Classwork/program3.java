5d7f68c405cef3f95c55417f7e7d72e390cef0c0
Shubham Kumawat <shubhamskumawat88@gmail.com>
2025-12-23 10:27:50 +0530

    Add files via upload

---
+++ Java_Programming/Classwork/program3.java
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////
+//
+//  Function Name : program3
+//  Description   : Used to print Jay Ganesh on screen
+//  Input         : String
+//  Output        : String
+//  Author        : Shubham Shankarlal kumawat
+//  Date          : 9/10/2025
+//
+/////////////////////////////////////////////////////////
+
+class program3
+{
+    public static void main(String A[])
+    {
+        System.out.println("Jay Ganesh.....");
+    }
+}
+
+//////////////////////////////////////////////////////////////
+// 
+//  Output : Jay Ganesh.....      
+//
+/////////////////////////////////////////////////////////////
